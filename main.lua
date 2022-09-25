d8a8dd46eb6b527cb4dd961a4d59c8f64abc5862
Huynh Quoc Khanh aka Kan <70838508+Kan-Kzeit@users.noreply.github.com>
2022-09-25 16:36:52 +0700

    update

--- main.lua
+++ main.lua
@@ -14,9 +14,14 @@ local tree3 = display.newImageRect("tree.png", 37*3, 49*3)
 tree3.x, tree3.y = display.contentCenterX - 70, display.contentCenterY + 30
 
 
+local bush = display.newImageRect("bush.png", 32 * 3, 22 * 3)
+bush.x, bush.y = 120, display.contentCenterY + 100
 tree.fill.effect = "filter.custom.sway"
 tree2.fill.effect = "filter.custom.sway"
 tree2.fill.effect.offset = 3.0
 
 tree3.fill.effect = "filter.custom.sway"
 tree3.fill.effect.offset = 2.0
+
+bush.fill.effect = "filter.custom.sway"
+bush.fill.effect.offset = 0.0
